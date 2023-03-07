ea3369b627b42abbdd5a4262b7ab34878bfa28d1
LucasNunesFerreira <lnunesferreira01@gmail.com>
2023-03-06 22:03:02 -0300

    pronto

---
+++ 4_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atv4</title>
+</head>
+<body>
+    
+<form action="./reds/red4.php" method="get">
+    <fieldset>
+        <legend>Insira uma letra</legend>
+        <input type="text" name="letra" id="letra"> <br>
+        <input type="submit" value="OK">
+    </fieldset>
+</form>
+
+</body>
+</html>
