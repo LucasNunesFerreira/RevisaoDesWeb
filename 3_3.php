52e48220a0256e544e2a0a222a2bb9967d0bc088
LucasNunesFerreira <lnunesferreira01@gmail.com>
2023-03-06 22:10:11 -0300

    mal feito, feito

---
+++ 3_3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="3_3-2.php" method="post">
+        <label for="num">Digite um número</label>
+        <input type="number" id="num" name="num" required>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
